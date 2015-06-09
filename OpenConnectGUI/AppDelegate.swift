92d213fb91fc5acc10fa95fdff9758422affcb3f
Hannu Kröger <hkroger@gmail.com>
2015-06-09 22:11:43 +0300

    Initial Commit

---
+++ OpenConnectGUI/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OpenConnectGUI
+//
+//  Created by Hannu Kröger on 9/6/2015.
+//  Copyright (c) 2015 Hannu Kröger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
