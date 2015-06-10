543c086e0bca8bb004fb31b9fd4e2b0c8d2835c8
Hannu Kröger <hkroger@gmail.com>
2015-06-10 14:40:38 +0300

    first tinkerings

--- OpenConnectGUI/AppDelegate.swift
+++ OpenConnectGUI/AppDelegate.swift
@@ -6,22 +6,82 @@
 //  Copyright (c) 2015 Hannu Kröger. All rights reserved.
 //
 
+// Icons by http://www.flaticon.com/authors/anton-saputro
+
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var settingsWindow: NSWindow!
+    @IBOutlet weak var settingsMenuItem: NSMenuItem!
 
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+   
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        notConnectedIcon()
+        statusItem.menu = statusMenu
+        settingsWindow.close()
+        settingsWindow.delegate = self
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    @IBAction func connectClicked(sender: NSMenuItem) {
+        if (sender.state == NSOffState){
+            sender.state = NSOnState
+            connectedIcon()
+        } else {
+            sender.state = NSOffState
+            notConnectedIcon()
+        }
     }
-
-
+    
+    @IBAction func settingsClicked(sender: NSMenuItem) {
+        if (sender.state == NSOffState){
+            sender.state = NSOnState
+            settingsWindow.makeKeyAndOrderFront(self)
+            NSApp.activateIgnoringOtherApps(true)
+        } else {
+            settingsWindow.close()
+        }
+    
+    }
+    
+    @IBAction func settingsSaveClicked(sender: NSButtonCell) {
+        closeSettings()
+    }
+    
+    @IBAction func settingsCancelClicked(sender: NSButtonCell) {
+        closeSettings()
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+    
+    func closeSettings() {
+        settingsWindow.close()
+    }
+    
+    func connectedIcon() {
+        let statusIconNotConnected = NSImage(named: "statusIconConnected")
+        statusIconNotConnected?.setTemplate(true)
+        statusItem.image = statusIconNotConnected
+    }
+    
+    func notConnectedIcon() {
+        let statusIconNotConnected = NSImage(named: "statusIconNotConnected")
+        statusIconNotConnected?.setTemplate(true)
+        
+        statusItem.image = statusIconNotConnected
+        
+    }
+    
+    func windowWillClose(notification: NSNotification) {
+        if ((notification.object as? NSWindow) == settingsWindow) {
+            settingsMenuItem.state = NSOffState
+        }
+    }
 }
 
